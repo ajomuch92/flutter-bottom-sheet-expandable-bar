3661b249fba2df357f2bc5a228f0f66bb91f8280
Aarón Montes <aaron@kssco.com>
2021-05-18 18:42:44 -0600

    Updating example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,9 +23,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,36 +50,47 @@ class MyHomePage extends StatelessWidget {
       bottomSheet: BottomBarSheet(
         buttonPosition: ButtonBottomBarPosition.center,
         innerChild: Center(child: Text('Bottom sheet'),),
+        currentIndex: _currentIndex,
+        curve: Curves.bounceIn,
         children: [
           BottomSheetBarIcon(
             icon: Icon(Icons.home),
             color: Colors.redAccent, 
             onTap: (){
+              setState(() {
+                _currentIndex = 0;
+              });
             },
           ),
           BottomSheetBarIcon(
             icon: Icon(Icons.person),
             color: Colors.blueAccent, 
             onTap: (){
+              setState(() {
+                _currentIndex = 1;
+              });
             },
           ),
           BottomSheetBarIcon(
             icon: Icon(Icons.edit),
             color: Colors.blue[800], 
             onTap: (){
+              setState(() {
+                _currentIndex = 2;
+              });
             },
           ),
           BottomSheetBarIcon(
             icon: Icon(Icons.star),
             color: Colors.orangeAccent, 
             onTap: (){
+              setState(() {
+                _currentIndex = 3;
+              });
             },
           ),
         ],
       ),
     );
   }
-}
-
-class BottomBarSheetIcon {
 }
